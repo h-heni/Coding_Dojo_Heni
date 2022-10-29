2d2639599a38f8162700853aee8f32ad39b2a251
h-heni <h.heni@hotmail.com>
2022-10-29 19:59:24 +0100

    initial

--- Python-Flask/Week1/Day5/core-assignment/Counter/server.py
+++ Python-Flask/Week1/Day5/core-assignment/Counter/server.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,redirect,session
 
 app=Flask(__name__)
 
-app.secret_key="Benny bob wuz heer."
+app.secret_key="my secret key."
 
 @app.route('/')
 def hello_world():
