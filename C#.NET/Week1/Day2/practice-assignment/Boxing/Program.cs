d39913022ac6962c57434cf1c3b38ae09bdef797
h-heni <h.heni@hotmail.com>
2022-12-20 18:22:44 +0100

    boxing

---
+++ C#.NET/Week1/Day2/practice-assignment/Boxing/Program.cs
@@ -0,0 +1,15 @@
+﻿List<Object> Boxing= new List<Object>();
+
+Boxing.Add(7);
+Boxing.Add(28);
+Boxing.Add(-1);
+Boxing.Add(true);
+Boxing.Add("chair");
+
+    int sum =0;
+foreach(var box in Boxing){
+if(box is int){
+    sum+=(int)box;
+}
+Console.WriteLine(sum);
+}
