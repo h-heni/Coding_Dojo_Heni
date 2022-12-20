9c37a498c03a2650497d4be4b7c610ea68862e2f
h-heni <h.heni@hotmail.com>
2022-12-20 20:36:05 +0100

    Human class

---
+++ C#.NET/Week1/Day2/practice-assignment/Human/Program.cs
@@ -0,0 +1,28 @@
+﻿class Human 
+{
+    public string Name ;
+    public int Strength;
+    public int Intelligence;
+    public int Dexterity;
+    public int Health;
+
+public Human (string name){
+    Name=name;
+    Strength=3;
+    Intelligence=3;
+    Dexterity=3;
+    Health=100;
+}
+public Human (string name,int strength,int intelligence,int dexterity,int health){
+    Name=name;
+    Strength=strength;
+    Intelligence=intelligence;
+    Dexterity=dexterity;
+    Health=health;
+}
+
+public int Attack (Human target){
+    target.Health-=(3*this.Strength);
+    return target.Health;
+}
+}
