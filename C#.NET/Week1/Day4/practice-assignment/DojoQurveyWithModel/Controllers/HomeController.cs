98fbe9f4d0e434b2bdab51506e5701b90c26d1d3
h-heni <h.heni@hotmail.com>
2022-12-24 21:12:36 +0100

    Model with Form

---
+++ C#.NET/Week1/Day4/practice-assignment/DojoQurveyWithModel/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoQurveyWithModel.Models;
+
+namespace DojoQurveyWithModel.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost("survey")]
+    public IActionResult Result (Survey yourSurvey)
+{
+    Console.WriteLine(yourSurvey);
+    List<Survey> sur = new List<Survey>();
+
+    sur.Add(yourSurvey);
+    return View("Result",sur);
+}
+
+
+    
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
