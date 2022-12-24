391ed7f8f24ce6624d611b54b310a54ab6dba260
h-heni <h.heni@hotmail.com>
2022-12-24 18:46:49 +0100

    view models practice

---
+++ C#.NET/Week1/Day4/practice-assignment/ViewModels/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ViewModels.Models;
+
+namespace ViewModels.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    public List<User> UsersName(){
+
+    return new List<User>()
+            {
+                new User(){ Name="heni"},
+                new User(){ Name="iheb"},
+                new User(){ Name="nassim"},
+                new User(){ Name="anwer"},
+            };
+    }
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        string msg = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quos hic adipisci aut animi beatae iste reiciendis, officiis ad cupiditate itaque nam sapiente, ratione distinctio laboriosam ea officia, libero dolore?";
+        return View("Index",msg);
+    }
+
+    [HttpGet("numbers")]
+    public IActionResult Numbers()
+    {
+        int [] num ={1,2,5,7,9,25};
+        return View(num);
+    }
+
+    [HttpGet("users")]
+    public IActionResult Users()
+    {
+    List<User> users =UsersName();
+    return View(users);
+    }
+    [HttpGet("user")]
+    public IActionResult User()
+    {
+        Random rand= new Random();
+    List<User> users =UsersName();
+
+        var user=users[rand.Next(0,users.Count)];
+
+        return View(user);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ C#.NET/Week1/Day4/practice-assignment/ViewModels/Models/User.cs
@@ -0,0 +1,8 @@
+#pragma warning disable CS8618
+
+namespace ViewModels.Models;
+public class User
+{
+    public string Name { get; set; }
+
+}
