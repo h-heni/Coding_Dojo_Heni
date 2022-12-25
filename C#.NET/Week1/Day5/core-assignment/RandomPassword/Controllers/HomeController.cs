c8a6194da317b156335ba45145c015c0696c1ed0
h-heni <h.heni@hotmail.com>
2022-12-25 16:31:16 +0100

    Random Password

---
+++ C#.NET/Week1/Day5/core-assignment/RandomPassword/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RandomPassword.Models;
+
+namespace RandomPassword.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    private static Random random = new Random();
+
+    public static string RandomString(int length)
+{
+    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    return new string(Enumerable.Repeat(chars, length)
+        .Select(s => s[random.Next(s.Length)]).ToArray());
+}
+    public IActionResult Index()
+    {
+        HttpContext.Session.SetString("Random", RandomString(14));
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Privacy()
+    {
+
+        return View("Index");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
